d7e57acb516bcf53edf7cc3a1a85273a9e496a6f
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-05-25 14:24:24 +0530

    App.jsx updated.

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './pages/Team5/Navbar';
+import Home from './pages/Team5/Home';
+import RegisterPage from './pages/Team5/RegisterPage';
+import LoginPage from './pages/Team5/LoginPage';
+import ProfilePage from './pages/Team5/ProfilePage';
+import AdminPage from './pages/Team5/AdminPage';
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/admin" element={<AdminPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;

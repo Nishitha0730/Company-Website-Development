98ef12d7e47ada17539bbfdcaf9cd4c5d7eb3221
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-05-25 21:26:01 +0530

    AuthController.java updated.

---
+++ backend/src/main/java/com/company/website/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.company.website.controller;
+
+import com.company.website.dto.LoginRequest;
+import com.company.website.dto.Response;
+import com.company.website.dto.UserDto;
+import com.company.website.entity.User;
+import com.company.website.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Response register(@RequestBody User user) {
+        UserDto dto = userService.register(user);
+        return new Response("User registered", dto);
+    }
+
+    @PostMapping("/login")
+    public Response login(@RequestBody LoginRequest request) {
+        UserDto dto = userService.login(request);
+        return new Response("Login successful", dto);
+    }
+}

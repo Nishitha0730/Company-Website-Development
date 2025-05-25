6f8f26d8db191df72c5020d94a8492b7cdfb32db
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-05-25 18:20:40 +0530

    Navbar.jsx file updated & Navbar.css file created & updated

--- frontend/src/pages/Team5/Navbar.jsx
+++ frontend/src/pages/Team5/Navbar.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
-const Navbar = () => (
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/admin">Admin</Link></li>
-        </ul>
-    </nav>
-);
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1 className="logo">CompanySite</h1>
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/admin">Admin</Link></li>
+            </ul>
+        </nav>
+    );
+};
 
 export default Navbar;

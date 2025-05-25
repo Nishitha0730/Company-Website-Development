a33c47f430a532da26c23ae21aff2b9b44cdee06
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-05-25 21:23:57 +0530

    EntityDtoMapper.java updated.

---
+++ backend/src/main/java/com/company/website/mapper/EntityDtoMapper.java
@@ -0,0 +1,14 @@
+package com.company.website.mapper;
+
+import com.company.website.dto.UserDto;
+import com.company.website.entity.User;
+
+public class EntityDtoMapper {
+    public static UserDto toDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setUsername(user.getUsername());
+        dto.setEmail(user.getEmail());
+        dto.setRole(user.getRole());
+        return dto;
+    }
+}

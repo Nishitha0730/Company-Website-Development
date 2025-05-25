52465cd1c47aab9a8ce1d608a38fcb0332b82b42
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-05-25 21:26:30 +0530

    UserController.java updated.

---
+++ backend/src/main/java/com/company/website/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.company.website.controller;
+
+import com.company.website.dto.Response;
+import com.company.website.dto.UserDto;
+import com.company.website.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile/{username}")
+    public Response getProfile(@PathVariable String username) {
+        UserDto dto = userService.getProfile(username);
+        return new Response("Profile fetched", dto);
+    }
+}
